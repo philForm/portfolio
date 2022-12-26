880e98987aadf1948be9765a8650a234e8ff0d31
Philippe <phil-form@orange.fr>
2022-12-26 07:20:25 +0100

    Header : transition du menu déroulant en mode mobile

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,27 @@
 const toggle = document.getElementById("header-toggle");
 const nav = document.getElementById("nav");
 const navLink = document.getElementsByClassName("nav");
-console.log(navLink[0]);
+const navUl = document.querySelector("#nav > ul");
+console.log(navUl);
 
 toggle.addEventListener("click", () => {
-    nav.classList.toggle("bloc");
+
+    if (!navUl.classList.contains("transition")) {
+        navUl.classList.add("transition");
+        navUl.style.zIndex = -20;
+
+    } else {
+        navUl.classList.remove("transition");
+        setTimeout(() => {
+            navUl.style.zIndex = 0;
+        }, 500)
+    }
 });
 
 for (let i = 0; i < navLink.length; i++) {
     navLink[i].addEventListener("click", () => {
-        nav.classList.remove("bloc");
+        navUl.style.zIndex = -20;
+        navUl.classList.add("transition");
+
     });
 };
