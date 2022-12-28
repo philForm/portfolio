49fac43948251b0d26429fa5ca32bccd0726a1d8
Philippe <phil-form@orange.fr>
2022-12-28 06:30:18 +0100

    Mise en places des différentes sections, et effet scroll-snap.

--- JS/script.js
+++ JS/script.js
@@ -5,8 +5,6 @@ const navUl = document.querySelector("#nav > ul");
 console.log(navUl);
 console.log(window.innerWidth)
 
-
-
 window.addEventListener("resize", () => {
     if (window.matchMedia("(min-width: 500px)").matches && !navUl.classList.contains("transition")) {
         navUl.classList.add("transition");
@@ -36,3 +34,25 @@ for (let i = 0; i < navLink.length; i++) {
 
     });
 };
+
+// ===================================================
+
+const header = document.querySelector(".header");
+const headerHeight = header.offsetHeight;
+
+// for (let i = 0; i < navLink.length; i++) {
+
+//     navLink[i].addEventListener("click", (e) => {
+//         e.preventDefault();
+//         let idTarget = navLink[i].getAttribute("href").replace("#", "");
+//         console.log(idTarget);
+//         const target = document.getElementById(`${idTarget}`);
+//         console.log(target);
+//         // target.style.background = "red";
+//         target.scrollTop = -100;
+//         // document.querySelector("html").animate({
+//         //     scrollTop: target.offset().top - headerHeight
+//         // }, 800);
+//         return false;
+//     })
+// }
